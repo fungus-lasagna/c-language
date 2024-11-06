93447b6b0ad7834397838da533569ce962df7697
fungus-lasagna <ytbookbug@gmail.com>
2024-11-06 17:41:14 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,18 @@
+Task Description
+請在螢幕上印出Hello WorLD!
+
+Input Format
+無輸入
+
+Output Format
+請在螢幕上印出Hello WorLD!
+
+
 #include <stdio.h>
 
 int main()
 {
-    printf("HELLO WORLD!");
+    printf("Hello WorLD!");
 
     return 0;
 }
