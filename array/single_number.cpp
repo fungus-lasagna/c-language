e35ffc5954d008b2655144dff92bd18632051ade
fungus-lasagna <ytbookbug@gmail.com>
2024-11-12 21:10:56 +0800

    Update single_number.cpp

--- array/single_number.cpp
+++ array/single_number.cpp
@@ -1,3 +1,9 @@
+Task Description
+Single Number
+給定一組長度為 n 的正整數陣列 A。陣列中只有一個元素只出現一次，其它元素都會出現兩次。請找出只出現一次的元素，並列印出來。
+
+
+
 #include <stdio.h>
 #include <string.h>
 
@@ -12,7 +18,8 @@ int main()
     {
         scanf("%d", &A[i]);
     }
-    
+
+    //利用 0 xor a = a, a xor a = 0的特性
     int xor_test = 0;
     for (int i = 0; i < n; i++)
     {
